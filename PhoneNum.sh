278270b4d032d8343ab94016cf7ef82acc04969e
Pigonhayo <hyewon4782@naver.com>
2024-06-07 04:27:38 +0000

    제발

--- PhoneNum.sh
+++ PhoneNum.sh
@@ -5,7 +5,7 @@ PHONEBOOK="phonebook.txt"
 
 # 입력 파라미터 확인
 if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <name> <phone_number>"
+    echo "인수를 2개만 입력해주세요."
     exit 1
 fi
 
@@ -14,20 +14,21 @@ name="$1"
 number="$2"
 
 # 번호가 숫자인지 확인
-if ! [[ "$number" =~ ^[0-9-]+$ ]]; then
+if ! [[ "$number" =~ ^[0-9-]+$ ]]; then # =~ 연산자는 문자열이 정규 표현식과 일치하는지 확인할 때 사용함
     echo "옳지 않은 전화번호입니다."
     exit 1
 fi
 
 # 지역번호 구현
-area_code=$(echo "$number" | cut -c1-3)
+area_code=$(echo "$number" | cut -d'-' -f1 )
 case $area_code in
     "02") area="서울" ;;
     "051") area="부산" ;;
     "053") area="대구" ;;
     "032") area="인천" ;;
     "062") area="광주" ;;
-    *) echo "Unknown area code." ; exit 1 ;;
+    "055") area="경남" ;;
+    *) echo "존재하지 않는 지역번호입니다." ; exit 1 ;; # 여기서 *은 default와 같이 동작 
 esac
 
 # 전화번호부 검색 및 수정
@@ -42,7 +43,7 @@ if [ -f "$PHONEBOOK" ]; then
                 exit 0
             else
                 echo "$name의 전화번호를 업데이트합니다."
-                sed -i "/^$name/d" "$PHONEBOOK"  # $name과 일치하는 줄을 $PHONEBOOK 파일에서 삭제
+                sed -i "/^$name/d" "$PHONEBOOK" # $name과 일치하는 줄을 $PHONEBOOK 파일에서 삭제
                 echo "$name $number $area" >> "$PHONEBOOK"
                 exit 0
             fi
@@ -52,7 +53,8 @@ fi
 
 # 새로운 항목 추가
 echo "$name으로 새로운 항목을 추가합니다."
-echo "$name $number $area" >> "$PHONEBOOK"
+echo "$name $number $area" >> "$PHONEBOOK" # >>는 출력 리다이렉션 연산자로, 기존 파일에 내용을 추가(append)할 때 사용
+sort -o "$PHONEBOOK" "$PHONEBOOK" # 파일을 가나다순으로 정렬
 exit 0
 
 
